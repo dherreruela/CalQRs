f82e3c82f0ec4b7245434672ddd09cc4386669c1
dherreruela <144584803+dherreruela@users.noreply.github.com>
2023-09-10 08:25:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyaLhE7RLCCqL1UukPtsQhKUqpITkaBJeuY6N7Le_cEfF38yPW-YIWo3OsTaa-34ThZvw/exec";
